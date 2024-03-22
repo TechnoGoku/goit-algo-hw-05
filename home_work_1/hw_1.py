758558f5ffffa98643f70ba04fdd55bbc8be0bbc
TechnoGoku <bulatikilya@gmail.com>
2024-03-22 23:47:47 +0200

    update_2

---
+++ home_work_1/hw_1.py
@@ -0,0 +1,19 @@
+# ФУНКЦІЯ caching_fibonacci
+#     Створити порожній словник cache
+
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        
+
+#     ФУНКЦІЯ fibonacci(n)
+#         Якщо n <= 0, повернути 0
+#         Якщо n == 1, повернути 1
+#         Якщо n у cache, повернути cache[n]
+
+#         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+#         Повернути cache[n]
+
+#     Повернути функцію fibonacci
+# КІНЕЦЬ ФУНКЦІЇ caching_fibonacci
