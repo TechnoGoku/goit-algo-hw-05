7a8d6d75df42fe06429be95fef710ffec0839f43
TechnoGoku <bulatikilya@gmail.com>
2024-03-23 16:09:47 +0200

    update_3

--- home_work_2/hw_2.py
+++ home_work_2/hw_2.py
@@ -4,7 +4,9 @@ from typing import Callable
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 
 def generator_numbers(text: str):
-    pass
+    numbers_text = []
+    for numbers in text:
+        
 
 def sum_profit(text: str, func: Callable):
     pass
