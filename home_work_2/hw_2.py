6199f4a9e2310da1346aadc2b3754ac237eb2d91
TechnoGoku <bulatikilya@gmail.com>
2024-03-23 16:18:11 +0200

    update_4

--- home_work_2/hw_2.py
+++ home_work_2/hw_2.py
@@ -1,12 +1,14 @@
 import re
 from typing import Callable
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 
 def generator_numbers(text: str):
     numbers_text = []
-    for numbers in text:
-        
+    numbers = re.findall(r"\d+\.\d+", text)
 
-def sum_profit(text: str, func: Callable):
-    pass
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+# def sum_profit(text: str, func: Callable):
+#     pass
+        
+ 
