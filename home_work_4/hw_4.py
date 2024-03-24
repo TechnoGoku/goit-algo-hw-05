1029596204a87d65769092d47cceb6a9038114d6
TechnoGoku <bulatikilya@gmail.com>
2024-03-24 15:41:23 +0200

    update_8

--- home_work_4/hw_4.py
+++ home_work_4/hw_4.py
@@ -1,20 +1,38 @@
+# ValueError add
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Give me name please."
+        except:
+            pass
+
+    return inner
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+@input_error
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+@input_error
 def change_contact(args, contacts):
     name, phone = args
-    if name in contacts:
-        raise ValueError("Contact")
+    if name not in contacts:
+        raise ValueError("Contact does not exist.")
     contacts[name] = phone
     return "Contact updated successfully"
 
+@input_error
 def show_contact(args, contacts):
     name = args[0]
     if name in contacts:
